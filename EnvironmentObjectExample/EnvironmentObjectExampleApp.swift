9574350dcc286f7c05b9f65081701e2e41b480ba
mohamed osama <mohamed.osama9812@gmail.com>
2023-12-10 18:07:11 +0200

    Initial Commit

---
+++ EnvironmentObjectExample/EnvironmentObjectExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnvironmentObjectExampleApp.swift
+//  EnvironmentObjectExample
+//
+//  Created by Mohamed Osama on 10/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct EnvironmentObjectExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
