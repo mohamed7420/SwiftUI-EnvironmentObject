09e3e591de742096a2733aede9417d84d9b8afd0
mohamed osama <mohamed.osama9812@gmail.com>
2023-12-10 18:25:49 +0200

    simple example of how to share data between views

--- EnvironmentObjectExample/ContentView.swift
+++ EnvironmentObjectExample/ContentView.swift
@@ -7,18 +7,47 @@
 
 import SwiftUI
 
+class DataExample: ObservableObject {
+    @Published var title: String = "Intial Title"
+    @Published var description: String = "Intial Description"
+}
+
 struct ContentView: View {
+    @EnvironmentObject var data: DataExample
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Hello, iOS Developers")
+            Text("\(data.title) \n\(data.description)")
+                .multilineTextAlignment(.center)
+            EnvironmentView()
+            AnotherEnvironmentView()
         }
+        .environmentObject(data)
         .padding()
+    }
+}
+
+struct EnvironmentView: View {
+    @EnvironmentObject var data: DataExample
+    var body: some View {
+        Button("Update Some Attributes") {
+            data.title = "New Title Value"
+            data.description = "Hello, Mohamed Osama, You do a great job👏"
+        }.environmentObject(data)
+    }
+}
+
+struct AnotherEnvironmentView: View {
+    @EnvironmentObject var data: DataExample
+    var body: some View {
+        VStack {
+            Text("\(data.title) \n \(data.description)")
+                .multilineTextAlignment(.center)
+        }.environmentObject(data)
     }
 }
 
 #Preview {
     ContentView()
+        .environmentObject(DataExample())
 }
